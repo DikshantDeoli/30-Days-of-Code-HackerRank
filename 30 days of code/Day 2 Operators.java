c7fd6c1e6bb19343094f96bbec85ed0fefa7e11c
Dikshant Deoli <69298906+DikshantDeoli@users.noreply.github.com>
2021-02-14 01:05:54 +0530

    Add files via upload

---
+++ 30 days of code/Day 2 Operators.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+           double s1 = (meal_cost/100)*tip_percent;
+           double s2 = (meal_cost/100)*tax_percent;
+           double sum = meal_cost +s1+s2;
+           System.out.println(Math.round(sum));
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double meal_cost = scanner.nextDouble();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tip_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tax_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+}
